8c9be71738e9cadb7f833c852e467789c5e60243
McKay1717 <nicolas1706ni@gmail.com>
2018-03-17 10:59:21 +0100

    Add Test Case

--- test/models/town_test.rb
+++ test/models/town_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class TownTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+ # test "the truth" do
+ #   assert true
+ # end
+ test "town validation does geocoding" do
+   belfort = Town.new
+   belfort.name = 'belfort'
+   assert belfort.valid?
+   assert_equal(47.6379599, belfort.lat)
+   assert_equal(6.8628942, belfort.lon)
+ end
+ 
+ test "town does not exist" do
+   unknown = Town.new
+   unknown.name = 'MyString'
+   assert !unknown.valid?
+ end
