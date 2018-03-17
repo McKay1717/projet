ce4f994a845503775f97d3f07a74445f6afc720b
McKay1717 <nicolas1706ni@gmail.com>
2018-03-17 11:03:21 +0100

    Correct Town test case (typo)

--- test/models/town_test.rb
+++ test/models/town_test.rb
@@ -6,14 +6,15 @@ class TownTest < ActiveSupport::TestCase
  # end
  test "town validation does geocoding" do
    belfort = Town.new
-   belfort.name = 'belfort'
+   belfort.town_name = 'belfort'
    assert belfort.valid?
-   assert_equal(47.6379599, belfort.lat)
-   assert_equal(6.8628942, belfort.lon)
+   assert_equal(47.6379599, belfort.latitude)
+   assert_equal(6.8628942, belfort.longitude)
  end
  
  test "town does not exist" do
    unknown = Town.new
-   unknown.name = 'MyString'
+   unknown.town_name = 'MyString'
    assert !unknown.valid?
  end
+end
