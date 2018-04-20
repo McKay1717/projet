51a27eae4e82a14081f3e3339b17167ecb3cb177
Nicolas <nicolas1706ni@gmail.com>
2018-04-20 20:25:58 +0200

    Allow cross origin

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Projet
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => 'https://mckay1717.github.io',
+    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+  }
   end
 end
